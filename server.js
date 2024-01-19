754253a9875fccb0c513658e66f36353d9564b19
milenapupillo <milenapupillo@gmail.com>
2024-01-19 09:26:31 -0500

    Configured server.js for deployment

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ const app = express();
 
 /* Configure the app to refresh the browser when nodemon restarts
 --------------------------------------------------------------- */
-const liveReloadServer = livereload.createServer();
-liveReloadServer.server.once("connection", () => {
-    // wait for nodemon to fully restart before refreshing the page
-    setTimeout(() => {
-        liveReloadServer.refresh("/");
-    }, 100);
-});
+// const liveReloadServer = livereload.createServer();
+// liveReloadServer.server.once("connection", () => {
+//     // wait for nodemon to fully restart before refreshing the page
+//     setTimeout(() => {
+//         liveReloadServer.refresh("/");
+//     }, 100);
+// });
 
 
 /* Configure the app (app.set)
@@ -43,8 +43,30 @@ app.set('views', path.join(__dirname, 'views'));
 
 /* Middleware (app.use)
 --------------------------------------------------------------- */
+// Detect if running in a dev environment
+if (process.env.ON_HEROKU === 'false') {
+    // Configure the app to refresh the browser when nodemon restarts
+    const liveReloadServer = livereload.createServer();
+    liveReloadServer.server.once("connection", () => {
+        // wait for nodemon to fully restart before refreshing the page
+        setTimeout(() => {
+        liveReloadServer.refresh("/");
+        }, 100);
+    });
+    app.use(connectLiveReload());
+}
+
+// Body parser: used for POST/PUT/PATCH routes: 
+// this will take incoming strings from the body that are URL encoded and parse them 
+// into an object that can be accessed in the request parameter as a property called body (req.body).
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
-app.use(connectLiveReload());
+// Allows us to interpret POST requests from the browser as another request type: DELETE, PUT, etc.
+app.use(methodOverride('_method'));
+
+
+app.use(express.static('public'))
+// app.use(connectLiveReload());
 // Body parser: used for POST/PUT/PATCH routes: 
 // this will take incoming strings from the body that are URL encoded and parse them 
 // into an object that can be accessed in the request parameter as a property called body (req.body).
@@ -73,19 +95,19 @@ app.get('/about', function (req, res) {
 
 
 // When a GET request is sent to `/seed`, the destinations collection is seeded
-app.get('/seed', function (req, res) {
-    // Remove any existing destinations
-    db.Destination.deleteMany({})
-        .then(removedDestinations => {
-            console.log(`Removed ${removedDestinations.deletedCount} destinations`)
-            // Seed the destinations collection with the seed data
-            db.Destination.insertMany(db.seedDestinations)
-                .then(addedDestinations => {
-                    console.log(`Added ${addedDestinations.length} destinations`)
-                    res.json(addedDestinations)
-                })
-        })
-});
+// app.get('/seed', function (req, res) {
+//     // Remove any existing destinations
+//     db.Destination.deleteMany({})
+//         .then(removedDestinations => {
+//             console.log(`Removed ${removedDestinations.deletedCount} destinations`)
+//             // Seed the destinations collection with the seed data
+//             db.Destination.insertMany(db.seedDestinations)
+//                 .then(addedDestinations => {
+//                     console.log(`Added ${addedDestinations.length} destinations`)
+//                     res.json(addedDestinations)
+//                 })
+//         })
+// });
 
 
 // This tells our app to look at the `controllers/destinations.js` file 
